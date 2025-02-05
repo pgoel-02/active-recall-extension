313bc72d8f5a6e2e85b22b7a5088c89f64907a12
Prashansa Goel <goel.prashansa@gmail.com>
2025-02-05 01:14:04 -0800

    Updated to render quiz after initial input received

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import IntroQuestion from "./components/IntroQuestion.jsx";
+import IntroQuestion from "./components/IntroQuestion";
+import TimedQuiz from "./components/TimedQuiz";
+import QuizAtEnd from "./components/QuizAtEnd";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <IntroQuestion />
-  </StrictMode>
-);
+function App() {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const handleAnswerChange = (answer) => {
+    setSelectedAnswer(answer);
+  };
+
+  return (
+    <StrictMode>
+      <div>
+        {!selectedAnswer ? (
+          <IntroQuestion onAnswerChange={handleAnswerChange} />
+        ) : selectedAnswer === "End" ? (
+          <QuizAtEnd selectedAnswer={selectedAnswer} />
+        ) : (
+          <TimedQuiz selectedAnswer={selectedAnswer} />
+        )}
+      </div>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
