97f92f3f5bb58471166100a028464666469bfa3d
Prashansa Goel <goel.prashansa@gmail.com>
2025-02-07 05:40:16 -0800

    Removed unnecessary imports

--- react-app/src/components/QuizAtEnd.jsx
+++ react-app/src/components/QuizAtEnd.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ListOfQuestions from "./ListOfQuestions";
 import useGetAllData from "../hooks/useGetAllData";
 
